e2c73965babd70cfc2ffda655f0623faeeca15dc
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2024-08-19 01:18:50 +0300

    ignore ai for now

--- ai/ask_ai.py
+++ ai/ask_ai.py
@@ -3,12 +3,13 @@ from dotenv import load_dotenv
 import openai
 
 load_dotenv()
-# client = openai.Client(api_key=os.getenv("OPENAI_API_KEY"))
+
 
 assistants_dict = eval(os.getenv("ASSISTANTS"))
 
 
 def generate_response(query, grade) -> str:
+    # client = openai.Client(api_key=os.getenv("OPENAI_API_KEY"))
     assistant = assistants_dict[grade]
 
     return ""
