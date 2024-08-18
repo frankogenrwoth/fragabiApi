a97272eca8df0f137eff54be862f600a8686085f
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2024-08-18 23:11:22 +0300

    add * to allow all hosts

--- fragabiApi/settings.py
+++ fragabiApi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a17dlv9!_r-6##i(y5&u^450ur-nx5@1x=tfl_le@8f-0++mdi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
