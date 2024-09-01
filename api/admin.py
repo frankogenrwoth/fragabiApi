39a3936e51793475e84fb345b1e994cff1097339
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2024-09-01 09:33:19 +0300

    add admin models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Question, Answer, AssignmentQuestion, Assignment, Consultation, FragabiUser
+
+
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Assignment)
+admin.site.register(AssignmentQuestion)
+admin.site.register(Consultation)
+admin.site.register(FragabiUser)
