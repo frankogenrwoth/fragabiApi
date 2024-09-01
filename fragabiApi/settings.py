2ae031b9377fdabb4fb01c1f032aa0a43feb63a0
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2024-09-01 09:29:06 +0300

    add django jazzmin and static files

--- fragabiApi/settings.py
+++ fragabiApi/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
