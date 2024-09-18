b135b0b8feea0f7bd22926bea0ac95df7972f158
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2024-09-18 16:22:56 +0300

    send email corrections after the assignment is complete

--- api/v1/views.py
+++ api/v1/views.py
@@ -46,20 +46,15 @@ class QuizViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'])
     def view(self, request):
         quiz_id: int = request.query_params.get('quiz_id')
-        curr: bool = request.query_params.get('curr', False)
 
         assignment = Assignment.objects.get(id=quiz_id)
         serializer = AssignmentSerializer(assignment)
 
-        if curr:
-            return Response(serializer.data)
 
         if not EmailStat.objects.filter(assignment=assignment).exists():
             if send_mark_sheet(assignment):
                 EmailStat.objects.create(assignment=assignment)
 
-
-        # print(assignment, serializer.data)
         return Response(serializer.data)
 
     @action(detail=False, methods=['get'])
