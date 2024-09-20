ea53681c227627a1ed212b4699ecd51175356d52
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2024-09-20 18:02:25 +0300

    add csrf exempt

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path, include
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.routers import DefaultRouter
 from .views import FragabiUserViewSet, QuizViewSet, ConsultationViewSet
 
-router = DefaultRouter()
+
+class CSRFExemptDefaultRouter(DefaultRouter):
+    def get_urls(self):
+        urls = super().get_urls()
+        return [path(url.pattern, csrf_exempt(url.callback), name=url.name) for url in urls]
+
+
+router = CSRFExemptDefaultRouter()
 router.register(r'users', FragabiUserViewSet)
 router.register(r'assignments', QuizViewSet)
 router.register(r'consultations', ConsultationViewSet)
-
 
 urlpatterns = [
     path('v1/', include(router.urls)),
